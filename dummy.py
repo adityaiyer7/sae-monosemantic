cce9a042d362a04689d820691c9636cfaebd8da2
Aditya Iyer <adityaiyer.m@gmail.com>
2026-02-28 20:48:30 -0800

    added basic value analysis methods (WIP)

--- dummy.py
+++ dummy.py
@@ -1,43 +1,43 @@
-# # dummy file used for scratch padding. 
-# import torch
+# # # dummy file used for scratch padding. 
+# # import torch
 
-# # Load the saved data
-# data = torch.load('sae_training_data.pt')
+# # # Load the saved data
+# # data = torch.load('sae_training_data.pt')
 
-# token_ids = data['token_ids']
-# attention_masks = data['attention_mask']
-# filtered_residuals = data['filtered_residual_activations']
+# # token_ids = data['token_ids']
+# # attention_masks = data['attention_mask']
+# # filtered_residuals = data['filtered_residual_activations']
 
 
-# print(filtered_residuals)
-import random
+# # print(filtered_residuals)
+# import random
 
-import wandb
+# import wandb
 
-# Start a new wandb run to track this script.
-run = wandb.init(
-    # Set the wandb entity where your project will be logged (generally your team name).
-    entity="adityaiyer-m-self",
-    # Set the wandb project where this run will be logged.
-    project="my-awesome-project",
-    # Track hyperparameters and run metadata.
-    config={
-        "learning_rate": 0.02,
-        "architecture": "CNN",
-        "dataset": "CIFAR-100",
-        "epochs": 10,
-    },
-)
+# # Start a new wandb run to track this script.
+# run = wandb.init(
+#     # Set the wandb entity where your project will be logged (generally your team name).
+#     entity="adityaiyer-m-self",
+#     # Set the wandb project where this run will be logged.
+#     project="my-awesome-project",
+#     # Track hyperparameters and run metadata.
+#     config={
+#         "learning_rate": 0.02,
+#         "architecture": "CNN",
+#         "dataset": "CIFAR-100",
+#         "epochs": 10,
+#     },
+# )
 
-# Simulate training.
-epochs = 10
-offset = random.random() / 5
-for epoch in range(2, epochs):
-    acc = 1 - 2**-epoch - random.random() / epoch - offset
-    loss = 2**-epoch + random.random() / epoch + offset
+# # Simulate training.
+# epochs = 10
+# offset = random.random() / 5
+# for epoch in range(2, epochs):
+#     acc = 1 - 2**-epoch - random.random() / epoch - offset
+#     loss = 2**-epoch + random.random() / epoch + offset
 
-    # Log metrics to wandb.
-    run.log({"acc": acc, "loss": loss})
+#     # Log metrics to wandb.
+#     run.log({"acc": acc, "loss": loss})
 
-# Finish the run and upload any remaining data.
-run.finish()
+# # Finish the run and upload any remaining data.
+# run.finish()
---
+++ src/evaluation/interpretability.py
@@ -0,0 +1,88 @@
+import sys
+import re
+import glob
+import os
+import torch
+from collections import defaultdict
+from typing import DefaultDict, Tuple, Optional
+from pathlib import Path
+from src.models.spare_autoencoder import SparseAutoEncoder
+
+
+
+
+project_root = Path.cwd() if (Path.cwd() / "src").exists() else Path.cwd().parent
+MODEL_SAVE_PATH = project_root / f'model_weights_4x.pth'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+activation_chunk_dir = str(project_root / 'data' / 'gpt2_activation_chunks')
+
+
+def natural_sort_key(path):
+    """Extract numbers from filename for proper numeric sorting."""
+    return [int(text) if text.isdigit() else text.lower() 
+            for text in re.split(r'(\d+)', path)]
+
+
+files = sorted(glob.glob(f"{activation_chunk_dir}/*.pt"), key = natural_sort_key)[:1]
+
+
+state_dict = torch.load(MODEL_SAVE_PATH, weights_only=True, map_location = 'cpu')
+
+
+model = SparseAutoEncoder(d_model=768, expansion_factor=4)
+model.load_state_dict(state_dict)
+model = model.to(device)
+model.eval()
+
+
+def get_active_features(x: torch.Tensor, threshold:float = 0.07)-> DefaultDict[int, float]:
+    '''
+    x is going to be a two dimensional tensor
+    '''
+    assert x.ndim == 1
+    active_idx = (x > threshold).nonzero(as_tuple = True)[0]
+    return dict(zip(active_idx.tolist(), x[active_idx].tolist()))
+
+
+def get_max_activating_features(feature_map:DefaultDict[int, float], top_k:Optional[int], sort_order:str='descending')->dict[int, float]:
+    assert(sort_order == 'descending' or sort_order == 'ascending')
+    is_descending = (sort_order == 'descending')
+    sorted_items = sorted(feature_map.items(), key = lambda x:x[1], reverse = is_descending)
+    if top_k:
+        top_k = min(top_k, len(sorted_items))
+        sorted_items = sorted_items[:top_k]
+    return dict[int, float](sorted_items)
+
+
+for chunk_file in files:
+    print(f"Loading chunk file: {chunk_file}")
+    chunk_data = torch.load(chunk_file, map_location=device)
+    token_ids = chunk_data["token_ids"]
+    activations = chunk_data["filtered_residual_activations"]
+
+    print("activations and token_ids extracted")
+
+    if token_ids.shape[0] != activations.shape[0]:
+        raise ValueError(
+            f"Mismatched lengths in {chunk_file}, "
+            f"token_id shape = {token_ids.shape[0]} vs "
+            f"activations shape = {activations.shape[0]}"
+        )
+    
+    for i in range(token_ids.shape[0]):
+        sample = {
+                    "token_ids": token_ids[i],
+                    "activations": activations[i]
+        }
+        print(sample["token_ids"])
+        print(sample["activations"].shape)
+
+        SAE_encoded_rep, results = model.forward(sample["activations"])
+        print("features", SAE_encoded_rep)
+        print("feature shape", SAE_encoded_rep.shape)
+        print("feature dim", SAE_encoded_rep.ndim)
+        active_feature_mapping = get_active_features(SAE_encoded_rep)
+
+        max_active_features = get_max_activating_features(active_feature_mapping, top_k = 10)
+        print(max_active_features)
+        break
